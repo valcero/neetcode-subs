442e8ae3ea18cb959ce3918f7ac3bee3a51a0628
valcero <134505680+valcero@users.noreply.github.com>
2026-06-03 03:43:41 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int count = 1;
+            int j = i + 1;
+            while (j < n) {
+                if (temperatures[j] > temperatures[i]) {
+                    break;
+                }
+                j++;
+                count++;
+            }
+            count = (j == n) ? 0 : count;
+            res[i] = count;
+        }
+        return res;
+    }
+}
